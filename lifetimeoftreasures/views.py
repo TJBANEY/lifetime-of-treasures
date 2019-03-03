1d9d6bf0c5c0ef31bb1ed0e37452afa0293911c7
Timothy Baney <baney-surv@hotmail.com>
2019-03-03 13:06:49 -0600

    Add less directory for styles

--- lifetimeoftreasures/views.py
+++ lifetimeoftreasures/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def health_check():
+    print("Healthy")
+    # return HttpResponse("Healthy")
 
 def landing_page(request):
     template = "base.html"
