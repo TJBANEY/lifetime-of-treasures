3138c184547dedd3881ac8daae0f989e53a4b75c
Timothy Baney <baney-surv@hotmail.com>
2019-03-03 13:41:19 -0600

    Edit wsgi file to add project to system path

--- lifetimeoftreasures/wsgi.py
+++ lifetimeoftreasures/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lifetimeoftreasures.settings.production')
+
+sys.path.append('/var/www/html/project/lifetime-of-treasures/')
+sys.path.append('/var/www/html/project/django/lib/python3.6/site-packages')
 
 application = get_wsgi_application()
