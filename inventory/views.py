c650f3110532dec400d7f6ce99f8bf9d0209f2ed
Timothy Baney <baney-surv@hotmail.com>
2019-03-17 00:19:48 -0500

    Add JS function to handle changing category filters

--- inventory/views.py
+++ inventory/views.py
@@ -1,13 +1,32 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
 
-from inventory.models import InventoryItem
+from inventory.models import InventoryItem, Category
 
+@csrf_exempt
+def change_category(request):
+    category_filter = request.POST.get('category')
+    category = Category.objects.filter(name__icontains=category_filter).first()
+
+    inventory = InventoryItem.objects.filter(category=category)
+    template = 'ajax/filtered_inventory.html'
+    context = {
+        'inventory': inventory
+    }
+
+    new_inventory = render_to_string(template, context, request)
+    return HttpResponse(json.dumps({'new_inventory': new_inventory}), content_type='application/json')
 
 def inventory_catalog(request):
     template = "catalog.html"
 
     inventory = InventoryItem.objects.all()
+    categories = Category.objects.all()
 
     # Pagination
     paginator = Paginator(inventory, 16)
@@ -22,7 +41,8 @@ def inventory_catalog(request):
 
     context = {
         'inventory': inventory,
-        'page_set': page_set
+        'page_set': page_set,
+        'categories': categories
     }
 
     return render(request, template, context)
--- lifetimeoftreasures/urls.py
+++ lifetimeoftreasures/urls.py
@@ -6,12 +6,13 @@ from django.conf.urls.static import static
 
 from filebrowser.sites import site
 
-from inventory.views import inventory_catalog
+from inventory.views import inventory_catalog, change_category
 from lifetimeoftreasures.views import landing_page
 
 urlpatterns = [
     path('admin/filebrowser/', site.urls),
     path('admin/', admin.site.urls),
     path('catalog/', inventory_catalog),
+    path('filter-change', change_category),
     path('', landing_page)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
