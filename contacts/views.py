73e71c30b1c155968b1f5e07b8240ad0bc548f4c
Timothy Baney <baney-surv@hotmail.com>
2019-05-26 21:22:32 -0500

    Create contact form template

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def contact_form(request):
+    template = 'contacts/contact.html'
+    context = {}
+
+    return render(request, template,  context)
